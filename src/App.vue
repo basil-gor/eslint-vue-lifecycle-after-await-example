96483ff5f1620b9cafafdf6acf22ea87f1ebaed4
Basil Goryachev <lagom@ecwid.com>
2022-12-01 16:26:46 +0400

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+// import { onMounted } from 'vue';
+
+const asyncFunction = async () => {
+  // some async logic
+}
+
+export default defineComponent({
+  async setup() {
+
+    await asyncFunction();
+
+    onMounted(() => {
+      // some hook logic
+    });
+  }
+})
+</script>
+
+<template>
+  Test component
+</template>
